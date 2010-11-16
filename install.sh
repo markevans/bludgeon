f420b5cb5d5f087c00ff6045c557b4508f1cdce1
Mark Evans <mark@new-bamboo.co.uk>
2010-11-16 11:39:31 +0000

    aaaoop

--- install.sh
+++ install.sh
@@ -8,13 +8,7 @@ fi
 bludgeon_dir=`pwd`
 home_dir=$HOME
 
-for file in .ackrc .bash_profile .gemrc .gitconfig; do
+for file in .ackrc .bash_profile .gemrc .gitconfig bin git-completion.sh; do
   echo "Linking $file"
   ln -s $bludgeon_dir/$file $home_dir
 done
-
-echo "Copying bin"
-cp -r bin $home_dir
-echo "Copying git-completion.sh"
-cp git-completion.sh $home_dir
-
